68a1784899f5e940d2db0971bca3ae4e100c7f34
Emanuele <phates89@gmail.com>
2021-04-10 14:26:23 +0200

    cleanup code

--- _repo_xml_generator.py
+++ _repo_xml_generator.py
@@ -1,11 +1,14 @@
-""" downloaded from http://xbmc-addons.googlecode.com/svn/addons/ """
-""" This is a modded version of the original addons.xml generator """
+""" 
+    downloaded from http://xbmc-addons.googlecode.com/svn/addons/
+    This is a modded version of the original addons.xml generator
 
-""" Put this version in the root folder of your repo and it will """
-""" zip up all add-on folders, create a new zip in your zips folder """
-""" and then update the md5 and addons.xml file """
+    Put this version in the root folder of your repo and it will 
+    zip up all add-on folders, create a new zip in your zips folder
+    and then update the md5 and addons.xml file
 
-""" Recoded by whufclee (info@totalrevolution.tv) """
+Recoded by whufclee (info@totalrevolution.tv) 
+"""
+
 
 import re
 import os
@@ -13,14 +16,16 @@ import shutil
 import hashlib
 import zipfile
 
+
 class Generator:
     """
         Generates a new addons.xml file from each addons addon.xml file
         and a new addons.xml.md5 hash file. Must be run from the root of
         the checked-out repo. Only handles single depth folder structure.
     """
+
     def __init__(self):
-# Create the zips folder if it doesn't already exist
+        # Create the zips folder if it doesn't already exist
         zips_path = ('zips')
         if not os.path.exists(zips_path):
             os.makedirs(zips_path)
@@ -33,7 +38,6 @@ class Generator:
         print("Finished updating addons xml and md5 files")
 
     def _create_zips(self, addon_id, version):
-        xml_path = os.path.join(addon_id, 'addon.xml')
         addon_folder = os.path.join('zips', addon_id)
         if not os.path.exists(addon_folder):
             os.makedirs(addon_folder)
@@ -41,11 +45,11 @@ class Generator:
         final_zip = os.path.join('zips', addon_id, '{0}-{1}.zip'.format(addon_id, version))
         if not os.path.exists(final_zip):
             print("NEW ADD-ON - Creating zip for: {0} v.{1}".format(addon_id, version))
-            zip = zipfile.ZipFile(final_zip, 'w', compression=zipfile.ZIP_DEFLATED )
+            zp = zipfile.ZipFile(final_zip, 'w', compression=zipfile.ZIP_DEFLATED)
             root_len = len(os.path.dirname(os.path.abspath(addon_id)))
-            
+
             ignore = ['.git', '.github', '.gitignore', '.DS_Store', 'thumbs.db', '.idea', 'venv']
-            
+
             for root, dirs, files in os.walk(addon_id):
                 # remove any unneeded git artifacts
                 for i in ignore:
@@ -60,16 +64,16 @@ class Generator:
                                 files.remove(f)
                             except:
                                 pass
-                
+
                 archive_root = os.path.abspath(root)[root_len:]
 
                 for f in files:
                     fullpath = os.path.join(root, f)
                     archive_name = os.path.join(archive_root, f)
-                    zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
-            
-            zip.close()
-            
+                    zp.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+
+            zp.close()
+
             # Copy over the icon, fanart and addon.xml to the zip directory
             copyfiles = ['icon.png', 'fanart.jpg', 'addon.xml']
             files = os.listdir(addon_id)
@@ -109,7 +113,8 @@ class Generator:
         # loop thru and add each addons addon.xml file
         for addon in addons:
             try:
-                if not os.path.isdir(addon) or addon == "zips" or addon.startswith('.'):
+                if (not os.path.isdir(addon) or addon == "repo" or addon == "zips"
+                        or addon.startswith('.')):
                     continue
                 _path = os.path.join(addon, "addon.xml")
                 xml_lines = open(_path, "r", encoding='utf-8').read().splitlines()
@@ -126,7 +131,7 @@ class Generator:
                     addon_xml += line.rstrip() + "\n"
                 addons_xml += addon_xml.rstrip() + "\n\n"
 
-                # Create the zip files                
+                # Create the zip files
                 self._create_zips(addon, version)
 
             except Exception as e:
@@ -134,11 +139,13 @@ class Generator:
 
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
-        self._save_file(addons_xml.encode('utf-8'), file=os.path.join('zips', 'addons.xml'), decode=True)
+        self._save_file(addons_xml.encode('utf-8'),
+                        file=os.path.join('zips', 'addons.xml'), decode=True)
 
     def _generate_md5_file(self):
         try:
-            m = hashlib.md5(open(os.path.join('zips', 'addons.xml'), 'r', encoding='utf-8').read().encode('utf-8')).hexdigest()
+            m = hashlib.md5(open(os.path.join('zips', 'addons.xml'), 'r',
+                                 encoding='utf-8').read().encode('utf-8')).hexdigest()
             self._save_file(m, file=os.path.join('zips', 'addons.xml.md5'))
         except Exception as e:
             print("An error occurred creating addons.xml.md5 file!\n{0}".format(e))
